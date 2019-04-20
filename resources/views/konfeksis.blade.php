5c23a8a58d73f65fbfb3540a0f8a13d0b3b0baeb
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2019-04-20 21:32:49 +0700

    add konfeksi filter

--- resources/views/konfeksis.blade.php
+++ resources/views/konfeksis.blade.php
@@ -22,10 +22,10 @@
 
 @section('content')
 <div class="container" style="margin: 20px auto;">
-	<input type="text" id="search-form" class="form-control mb-4" placeholder="Search Posts" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search Posts'">
-	<div class="row" style="clear: both">
+	<input type="text" id="search-form" class="form-control mb-4" placeholder="Cari Konfeksi" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Car Konfeksi'" onkeyup="filter()">
+	<div class="row" id="daftar-konfeksi" style="clear: both">
 		@foreach($konfeksis as $konfeksi)
-		<div class="col-lg-3 col-md-4">
+		<div class="col-lg-3 col-md-4 item-konfeksi">
 			<div class="single-product" style="height: 410px; overflow: hidden;">
 				<img class="img-fluid" src="/storage/{{$konfeksi->gambar}}" style="width: 100%; height: 150px;">
 				<div class="product-details">
@@ -39,4 +39,28 @@
 		@endforeach
 	</div>
 </div>
+@endsection
+
+@section('custom-js')
+	<script>
+		function filter(){
+    		// Declare variables
+    		var input, filter, ul, li, a, i, txtValue;
+    		input = document.getElementById('search-form');
+    		filter = input.value.toUpperCase();
+    		ul = document.getElementById("daftar-konfeksi");
+    		li = ul.getElementsByClassName('item-konfeksi');
+
+    		// Loop through all list items, and hide those who don't match the search query
+    		for (i = 0; i < li.length; i++) {
+    			a = li[i].getElementsByTagName("h6")[0];
+    			txtValue = a.textContent || a.innerText;
+    			if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    				li[i].style.display = "";
+    			} else {
+    				li[i].style.display = "none";
+    			}
+    		}
+		}
+	</script>
 @endsection
